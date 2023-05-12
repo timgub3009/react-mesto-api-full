b876d71632d057a6bc3a380f9a20af22486793f7
Gubeidulin Timur <timur3009@yandex.ru>
2023-05-12 18:46:57 +0300

    login problem

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -91,6 +91,8 @@ const login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
+          sameSite: 'None',
+          secure: true,
         })
         .send({ message: 'Аутентификация успешна!' });
     })
