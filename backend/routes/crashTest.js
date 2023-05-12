6566b899cdfe47fb2879204227e75c6b7f66df65
Gubeidulin Timur <timur3009@yandex.ru>
2023-05-12 22:02:38 +0300

    crash test fixed

--- backend/routes/crashTest.js
+++
@@ -1,9 +0,0 @@
-const crashTestRouter = require('express').Router;
-
-crashTestRouter.get('/', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
-module.exports = crashTestRouter;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-const crashTestRouter = require('./crashTest');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use('/crash-test', crashTestRouter);
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 router.use('*', (req, res, next) => {
